9dd5f8f1cdf330f267ca314eb458bf81171db36b
riteshpt27 <riteshpt27@gmail.com>
2023-01-17 22:50:25 +0530

    Marking object responsibilities using stereotype annotations - Service Annotation - Repository Annotation

--- src/main/java/repositories/impl/DBCommentRepository.java
+++ src/main/java/repositories/impl/DBCommentRepository.java
@@ -2,9 +2,10 @@ package repositories.impl;
 
 import models.Comment;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import repositories.CommentRepository;
 
-@Component
+@Repository
 public class DBCommentRepository implements CommentRepository {
     @Override
     public void storeComment(Comment comment) {
